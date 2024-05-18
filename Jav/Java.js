a9c144479ceb9091e4867cf46dab613314a2b912
Raycype <stephenabigailnneoma@gamil.com>
2024-05-18 20:59:39 +0100

    Javascript task 1

---
+++ Jav/Java.js
@@ -0,0 +1,47 @@
+let firstName = "Abigail";
+let lastName = "Stephen";
+let maritalStatus = "Single";
+let country = "Nigeria";
+let age = 20;
+
+console.log(firstName)
+console.log(lastName)
+console.log(maritalStatus)
+console.log(country)
+console.log(age)
+
+let a = 4
+let b = 3
+
+//Using if else
+
+if (a > b){
+    console.log("a is greater than b")
+}
+else{
+    console.log("a is less than b")
+}
+
+//Using ternary operator
+a > b ? console.log("a is greater than b") : console.log("a is less than b")
+
+// You can check if a number is divisible by 2 with a remainder using Modulus (%)
+
+console.log(a > b)
+console.log(a >= b)
+console.log(a < b)
+
+let c = "4"
+
+console.log(a == c)
+console.log(a === c)
+
+let d = 10
+let e = 12
+
+console.log(!a > b && d < e)
+console.log(!a > b && d > e)
+
+const theDate = new Date()
+console.log(theDate)
+
